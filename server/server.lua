7f4832fa37d3f52d9a3d4aa3eaf9154f8905730c
Diamond <81833678+0x98a@users.noreply.github.com>
2024-04-04 12:03:00 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,18 +1,15 @@
 -- -------------------------------------------------------------------------- --
 --                                  Variables                                 --
 -- -------------------------------------------------------------------------- --
-local calls = {}
-local callid = {}
+local calls, callid = {}, {}
 
--- Config load jobs for id
-for k, v in ipairs(Config.Jobs) do
-    callid[v] = 0
-end
-
--- Config load jobs
+-- Config load jobs for id and load the calls array for each job in config, and load the for the loadedJobs array which will do better performance
 for k, v in ipairs(Config.Jobs) do
     calls[v] = {}
+    callid[v] = 0
+    loadedJobs[v] = k
 end
+
 
 -- Test call
 -- RegisterCommand('test_call', function(src, args)
@@ -24,57 +21,36 @@ end
 -- -------------------------------------------------------------------------- --
 
 function AddCall(src, message, number, coords)
-    for k, v in ipairs(Config.Jobs) do
-        if v == number then
-            local ped = nil
+    if calls[number] ~= nil then
+        local playerCoords = nil
 
-            local playerCoords = nil
-            local identifier69 = nil
-            local phonenumber = nil
-
-            if src == nil then
-                if type(coords) == "table" then
-                    if coords.x == nil or coords.y == nil or coords.z == nil then
-                        print("Fejl: Ugyldige koordinater, brug vector3(x, y, z) eller {x = 0, y = 0, z = 0}")
-                        return
-                    end
-                    playerCoords = vector3(coords.x, coords.y, coords.z)
-                elseif type(coords) == "vector3" then
-                    playerCoords = coords
-                else
-                    print("Fejl: Ugyldige koordinater, brug vector3(x, y, z) eller {x = 0, y = 0, z = 0}")
-                end
-                identifier69 = nil
-                phonenumber = Config.AutomaticMessage
+        if src == nil then
+            if type(coords) == "vector3" or (type(coords) == "table" and coords.x and coords.y and coords.z) then
+                playerCoords = type(coords) == "vector3" and coords or vector3(coords.x, coords.y, coords.z)
             else
-                ped = GetPlayerPed(src)
-                playerCoords = GetEntityCoords(ped)
-                local xPlayer = GetPlayer(src)
-                if xPlayer then
-                    identifier69 = GetIdentifier(src)
-                    phonenumber = GetPhoneNumber(GetIdentifier(src))
-                else
-                    identifier69 = nil
-                    phonenumber = Config.AutomaticMessage
-                end
+                print("Fejl: Ugyldige koordinater, brug vector3(x, y, z) eller {x = 0, y = 0, z = 0}")
+                return
             end
-            local data = {
-                identifier = identifier69,
-                date = os.date("%x %X"),
-                message = message,
-                taken = nil,
-                deleted = false,
-                fromnumber = phonenumber,
-                number = number,
-                coords = playerCoords,
-                onCall = 0,
-                onCallPlayers = {},
-                id = callid[number] + 1
-            }
-            callid[number] = callid[number] + 1
-            table.insert(calls[number], data)
-            SendToAddCall(number, calls[number][callid[number]])
         end
+
+        local data = {
+            identifier = src ~= nil and GetIdentifier(src) or nil,
+            date = os.date("%x %X"),
+            message = message,
+            taken = nil,
+            deleted = false,
+            fromnumber = GetPlayer(src) and GetPhoneNumber(GetIdentifier(src)) or Config.AutomaticMessage,
+            number = number,
+            coords = src ~= nil and GetEntityCoords(GetPlayerPed(src)) or playerCoords,
+            onCall = 0,
+            onCallPlayers = {},
+            id = callid[number] + 1
+        }
+
+        callid[number] = callid[number] + 1
+        
+        table.insert(calls[number], data)
+        SendToAddCall(number, calls[number][callid[number]])
     end
 end
 
@@ -84,61 +60,109 @@ end
 
 RegisterNetEvent("visualz_opkaldsliste:server:takeCall")
 AddEventHandler("visualz_opkaldsliste:server:takeCall", function(id, number)
-    local xPlayer = GetPlayerFromIdentifier(calls[number][id].identifier)
-    local sourcePlayer = GetPlayer(source)
-    local identifier = GetIdentifier(source)
-    if sourcePlayer.job.name == number then
-        if identifier ~= nil and calls[number][id]["onCallPlayers"][identifier] == nil then
-            calls[number][id]["onCallPlayers"][identifier] = sourcePlayer.getName()
-            calls[number][id].onCall = calls[number][id].onCall + 1
-
-            if calls[number][id]["taken"] == nil then
-                calls[number][id]["taken"] = sourcePlayer.getName()
-                if Config.SendTakenMessage and calls[number][id].fromnumber ~= Config.AutomaticMessage and xPlayer ~= nil and xPlayer.source ~= nil then
-                    SendTakenMessage(calls[number][id].identifier, calls[number][id].number, calls[number][id].fromnumber, xPlayer)
+    if calls[number][id] ~= nil then
+        local xPlayer = GetPlayerFromIdentifier(calls[number][id].identifier)
+        local sourcePlayer = GetPlayer(source)
+        local identifier = GetIdentifier(source)
+    
+        -- Tjek om sourcen faktisk er en spiller, og hvis det er så tjek om han har jobbet.
+        if not sourcePlayer or sourcePlayer.job.name ~= number or not identifier then
+            return
+        end
+    
+        -- Cache tingene til locals, så vi ikke skal tilgå arrays flere gange og den måde bruge mindre in-memory
+        local currOpkald = calls[number][id]
+        local onCallPlayers = currOpkald["onCallPlayers"]
+    
+        if not onCallPlayers[identifier] then
+            onCallPlayers[identifier] = sourcePlayer.getName() -- Ikke standalone
+            currOpkald.onCall = currOpkald.onCall + 1
+    
+            if not currOpkald["taken"] then
+                currOpkald["taken"] = sourcePlayer.getName() -- Ikke standalone
+    
+                if Config.SendTakenMessage and currOpkald.fromnumber ~= Config.AutomaticMessage and xPlayer and xPlayer.source then
+                    SendTakenMessage(currOpkald.identifier, currOpkald.number, currOpkald.fromnumber, xPlayer)
                 end
             end
-
-            UpdateCall(number, calls[number][id]["taken"], calls[number][id].onCall, calls[number][id].onCallPlayers, id)
+    
+            UpdateCall(number, currOpkald["taken"], currOpkald.onCall, onCallPlayers, id)
         end
+    else
+        print("Fejl: Ugyldigt opkald blev parsed i takeCall eventet??")
+        return
     end
 end)
 
 RegisterNetEvent("visualz_opkaldsliste:server:dropCall")
 AddEventHandler("visualz_opkaldsliste:server:dropCall", function(id, number)
-    local xPlayer = GetPlayerFromIdentifier(calls[number][id].identifier)
-    local sourcePlayer = GetPlayer(source)
-    local identifier = GetIdentifier(source)
-    if sourcePlayer.job.name == number then
-        if identifier ~= nil and calls[number][id]["onCallPlayers"][identifier] ~= nil then
-            calls[number][id]["onCallPlayers"][identifier] = nil
-            calls[number][id].onCall = calls[number][id].onCall - 1
+    local opkald = calls[number][id]
+        
+    if opkald then
+        local sourcePlayer = GetPlayer(source)
 
-            UpdateCall(number, calls[number][id]["taken"], calls[number][id].onCall, calls[number][id].onCallPlayers, id)
+        if not sourcePlayer or sourcePlayer.job.name ~= number or then --Check på om spilleren FAKTISK har jobbet, eller det en ugyldig spiller??, gør det tidligt så den køre videre i koden, mangler check til at check om de faktisk har jobbet i config
+            return
         end
+
+        local identifier = GetIdentifier(source) -- få identifier efter vi har sikret os spilleren er gyldig
+        
+        if not identifier then --dog return hvis den ik kan få en identifier
+            return
+        end
+
+        local onCallPlayers = opkald["onCallPlayers"] --få opkaldsdata med onCallPlayers
+
+        if onCallPlayers[identifier] then --tjek om identifieren faktisk er på jobbet
+            onCallPlayers[identifier] = nil --fjern ham fra opkaldet
+            opkald.onCall = math.max(0, opkald.onCall - 1) -- sikre os at onCall ikke går ned under 0 med math.max (tror det er det ellers må det være math.ceil + 0.5)
+            
+            UpdateCall(number, opkald["taken"], opkald.onCall, onCallPlayers, id)
+        end
+    else 
+        print("Fejl: Ugyldigt opkald blev parsed i dropCall eventet??")
+        return
     end
 end)
 
 RegisterNetEvent("visualz_opkaldsliste:server:deleteCall")
 AddEventHandler("visualz_opkaldsliste:server:deleteCall", function(id, number)
     local sourcePlayer = GetPlayer(source)
-    if sourcePlayer.job.name == number then
-        calls[number][id]["deleted"] = true
+
+    if not sourcePlayer or sourcePlayer.job.name ~= number then --return hvis spilleren ikke er det rigtige job eller spilleren er ugyldig, mangler check til at check om de faktisk har jobbet i config
+        return
+    end
+
+    local opkald = calls[number][id]
+
+    if opkald then
+        opkald["deleted"] = true
         DeleteCall(number, id)
+    else
+        print("Fejl: Ugyldigt opkald parsed i deleteCall??")
     end
 end)
 
 RegisterNetEvent("visualz_opkaldsliste:server:deleteAll")
 AddEventHandler("visualz_opkaldsliste:server:deleteAll", function(number)
     local sourcePlayer = GetPlayer(source)
-    if sourcePlayer.job.name == number then
+
+    if not sourcePlayer or sourcePlayer.job.name ~= number then --return hvis spilleren ikke er det rigtige job eller spilleren er ugyldig, mangler check til at check om de faktisk har jobbet i config
+        return
+    end
+
+    local opkaldsArray = calls[number]
+
+    if opkaldsArray then
         calls[number] = {}
         callid[number] = 0
         DeleteAll(number, sourcePlayer)
+    else
+        print("Fejl: Ugyldigt job parsed i deleteAll??")
     end
 end)
 
-
+-- Mangler sikkerhed
 RegisterNetEvent("visualz_opkaldsliste:server:sendMessage")
 AddEventHandler("visualz_opkaldsliste:server:sendMessage", function(number, message, id)
     SendCallMessage(calls[number][id].identifier, number, calls[number][id].fromnumber, message)
